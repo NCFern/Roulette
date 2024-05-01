b1c353b3090ae41855bde7163c61d7633136c668
Nicholas Fernandez <Nickfer610@gmail.com>
2024-05-01 04:35:49 +0000

    can delete specific bet made

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-var money = 2000;
+var money = 2000
 
-var moneyBet = 100;
+var moneyBet = 100
 
-var previousBets = [];
+var previousBets = []
 
-var selectedBets = [];
+var selectedBets = []
 
 var betTable = [
   {
@@ -195,57 +195,57 @@ var betTable = [
     color: 'red',
     column: 'three',
   },
-];
+]
 
-var clickedBet;
+var clickedBet
 
-var betDisplay = document.createElement('div');
-betDisplay.setAttribute('class', 'betDisplay');
+var betDisplay = document.createElement('div')
+betDisplay.setAttribute('class', 'betDisplay')
 
-var betNumber = document.createElement('h2');
-betNumber.textContent = 'number';
+var betNumber = document.createElement('h2')
+betNumber.textContent = 'number'
 
-var betColor = document.createElement('h2');
-betColor.textContent = 'color';
+var betColor = document.createElement('h2')
+betColor.textContent = 'color'
 
-betDisplay.appendChild(betNumber);
-betDisplay.appendChild(betColor);
+betDisplay.appendChild(betNumber)
+betDisplay.appendChild(betColor)
 
-var betDisplayNumber = document.querySelector('.displayNumber');
+var betDisplayNumber = document.querySelector('.displayNumber')
 
-var renderDisplay = betDisplay;
-betDisplayNumber.appendChild(renderDisplay);
+var renderDisplay = betDisplay
+betDisplayNumber.appendChild(renderDisplay)
 
-var renderMoney = document.getElementById('money');
-renderMoney.innerHTML = money;
+var renderMoney = document.getElementById('money')
+renderMoney.innerHTML = money
 
-var button = document.getElementById('bet-button');
+var button = document.getElementById('bet-button')
 button.addEventListener('click', function () {
-  randomBet(betTable);
-});
+  randomBet(betTable)
+})
 
 function randomBet(array) {
-  if (!clickedBet) return;
-  money -= moneyBet;
-  var results = array[Math.floor(Math.random() * array.length)];
-  betNumber.textContent = results.number;
-  betColor.textContent = results.color;
+  if (!clickedBet) return
+  money -= moneyBet
+  var results = array[Math.floor(Math.random() * array.length)]
+  betNumber.textContent = results.number
+  betColor.textContent = results.color
 
   if (clickedBet.type === 'color') {
     if (clickedBet.value === results.color) {
-      money += moneyBet * 2;
+      money += moneyBet * 2
     }
   }
 
   if (clickedBet.type === 'divisible') {
     if (results.number !== '0' || results.number !== '00') {
-      renderMoney.innerHTML = money;
-      clickedBet = false;
-      return;
+      renderMoney.innerHTML = money
+      clickedBet = false
+      return
     }
-    var even = clickedBet.value === 'even' ? 0 : 1;
+    var even = clickedBet.value === 'even' ? 0 : 1
     if (parseInt(results.number, 10) % 2 === even) {
-      money += moneyBet * 2;
+      money += moneyBet * 2
     }
   }
 
@@ -255,18 +255,18 @@ function randomBet(array) {
       clickedBet.value === 'first' &&
       parseInt(results.number, 10) >= 1
     ) {
-      money += moneyBet * 2;
+      money += moneyBet * 2
     } else if (
       parseInt(results.number, 10) > 18 &&
       clickedBet.value === 'last'
     ) {
-      money += moneyBet * 2;
+      money += moneyBet * 2
     }
   }
 
   if (clickedBet.type === 'single') {
     if (results.number === clickedBet.value) {
-      money += moneyBet * 36;
+      money += moneyBet * 36
     }
   }
 
@@ -276,82 +276,52 @@ function randomBet(array) {
       clickedBet.value === 'dozen1' &&
       parseInt(results.number, 10) >= 1
     ) {
-      money += moneyBet * 3;
+      money += moneyBet * 3
     } else if (
       parseInt(results.number, 10) <= 24 &&
       parseInt(results.number, 10) > 12 &&
       clickedBet.value === 'dozen2'
     ) {
-      money += moneyBet * 3;
+      money += moneyBet * 3
     } else if (
       parseInt(results.number, 10) > 24 &&
       clickedBet.value === 'dozen3'
     ) {
-      money += moneyBet * 3;
+      money += moneyBet * 3
     }
   }
 
   if (clickedBet.type === 'column') {
     if (clickedBet.value === results.column) {
-      money += moneyBet * 3;
+      money += moneyBet * 3
     }
   }
 
-  renderMoney.innerHTML = money;
-  previousBets.push(results);
-  console.log(previousBets);
-  clickedBet = false;
+  renderMoney.innerHTML = money
+  previousBets.push(results)
+  selectedBets = []
+  console.log('previousBets: ', previousBets)
+  clickedBet = false
 }
 
-var clickBet = document.querySelectorAll('.clickBet');
+var clickBet = document.querySelectorAll('.clickBet')
 clickBet.forEach(function (element) {
   element.addEventListener('click', function () {
     clickedBet = {
       type: element.getAttribute('data-bet'),
       value: element.getAttribute('data-value'),
-    };
-    var isSelected = selectedBets.some(
-      (item) => item.value === clickedBet.value,
-    );
+    }
+    var isSelected = selectedBets.some(item => item.value === clickedBet.value)
 
     if (isSelected) {
-      selectedBets.splice(clickedBet);
+      var index = selectedBets.findIndex(
+        item => item.value === clickedBet.value,
+      )
+      selectedBets.splice(index, 1)
     } else {
-      selectedBets.push(clickedBet);
+      selectedBets.push(clickedBet)
     }
-    console.log(clickedBet);
-    console.log(selectedBets);
-  });
-});
-
-// var pastNumber = document.createElement('h5');
-// pastNumber.textContent = 'number';
-
-// var pastColor = document.createElement('h5');
-// pastColor.textContent = 'color';
-
-// var previousNumber = document.querySelector('.previousNumberResult');
-
-// var pastDisplay = document.createElement('div');
-// pastDisplay.setAttribute('class', 'pastDisplay');
-
-// var pastNumber = document.createElement('h5');
-// pastNumber.textContent = 'number';
-
-// var pastColor = document.createElement('h5');
-// pastColor.textContent = 'color';
-
-// pastDisplay.appendChild(pastNumber);
-// pastDisplay.appendChild(pastColor);
-
-// var showPastResult = document.querySelector('.previous-result');
-
-// var renderPastResult = pastDisplay;
-// showPastResult.appendChild(renderPastResult);
-
-// var accmulator = 0;
-// var initialValue = 0;
-
-// var multipleBets = selectedBets.reduce(
-//   (accmulator, previousBets) => accmulator + previousBets, initialValue,
-// )
+    console.log('clickedBet: ', clickedBet)
+    console.log('selectedBets: ', selectedBets)
+  })
+})
